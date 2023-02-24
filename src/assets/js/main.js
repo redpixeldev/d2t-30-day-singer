7a810c58d58f6433ddb5269bd869525fe90b30ef
Vivian Guillen <vivgui@MBP14.local>
2023-02-24 19:44:19 -0400

    Finish unlock page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -45,4 +45,12 @@ $(document).ready(function () {
 			},
 		],
 	});
+
+	$(".review-slider").slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+	});
 });
